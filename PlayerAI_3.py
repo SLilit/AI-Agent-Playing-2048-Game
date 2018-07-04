afc900e8f7a3856bbc36c5f3ee26664199eda8b5
LILIT <lilit555ai@gmail.com>
2018-07-03 23:20:51 -0600

    Add PlsyerAI.py

---
+++ PlayerAI_3.py
@@ -0,0 +1,41 @@
+from random import randint
+from BaseAI_3 import BaseAI
+import time
+import math
+
+
+class PlayerAI(BaseAI):
+
+    def __init__(self):
+        
+        
+    def getMove(self, grid):
+          
+
+               
+                    
+
+
+                
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+
+    
