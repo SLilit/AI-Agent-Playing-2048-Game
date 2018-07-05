bb2da405e970936c5f41d1727b57e687e44fbb27
LILIT <lilit555ai@gmail.com>
2018-07-05 11:44:47 -0600

    Add getMove function

--- PlayerAI_3.py
+++ PlayerAI_3.py
@@ -7,21 +7,41 @@ import math
 class PlayerAI(BaseAI):
 
     def __init__(self):
-        
+        self.points = [[6,5,4,3],[5,4,3,2],[4,3,2,1],[3,2,1,0]]
+        self.time = 0.0
         
     def getMove(self, grid):
-          
-
-               
-                    
-
-
+        max_move = None
+        self.time = time.clock()
+        moves = grid.getAvailableMoves()
+        cell = len(grid.getAvailableCells())
+        if cell <= 4:
+            depth = 6
+        elif cell <= 8:
+            depth = 4
+        else:
+            depth = 2
+        
+        
+        if len(moves)!= 0:
+            max_alpha = float('-inf')
+            min_beta = float('inf')
+            
+            for move in moves:
+                gridCopy = grid.clone()
                 
-                
+                if gridCopy.canMove([move]):
+                    gridCopy.move(move)
+                    alpha = self.minimize(gridCopy, max_alpha, min_beta, depth)
+                     
+                    if alpha > max_alpha:        
+                        max_alpha = alpha
+                        max_move = move
+                            
+        return max_move    
 
 
-
-
+    
 
 
 
